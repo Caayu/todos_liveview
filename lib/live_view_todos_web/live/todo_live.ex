544503cc83f8d23e8b2a726e1b7a67b6d9f769e0
caayu <henrycay@outlook.com>
2020-12-04 18:18:35 -0300

    Simple TODO application Phoenix with LiveView for my studies

---
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -0,0 +1,30 @@
+defmodule LiveViewTodosWeb.TodoLive do
+  use LiveViewTodosWeb, :live_view
+  alias LiveViewTodos.Todos
+
+  def mount(_params, _session, socket) do
+    Todos.subscrive()
+
+    {:ok, fetch(socket)}
+  end
+
+  def handle_event("add", %{"todo" => todo}, socket) do
+    Todos.create_todo(todo)
+
+    {:noreply, fetch(socket)}
+  end
+
+  def handle_event("toggle_done", %{"id" => id}, socket) do
+    todo = Todos.get_todo!(id)
+    Todos.update_todo(todo, %{done: !todo.done})
+    {:noreply, fetch(socket)}
+  end
+
+  def handle_info({Todos, [:todo | _], _}, socket) do
+    {:noreply, fetch(socket)}
+  end
+
+  defp fetch(socket) do
+    assign(socket, todos: Todos.list_todos())
+  end
+end
